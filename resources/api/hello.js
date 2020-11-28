3259e7399bb1c725afe146119330c8bb4341bbe9
kondoumh <kondoumh@gmail.com>
2020-11-28 17:58:20 +0900

    add fetch option

--- resources/api/hello.js
+++ resources/api/hello.js
@@ -1,10 +1,28 @@
 exports.handler = async (e, c) => {
-  console.log(e);
-  console.log(c);
-  const fetch = require('node-fetch').default;
-  const url = "https://scrapbox.io/api/pages/kondoumh?limit=10";
-  const res = await fetch(url);
+  const project = e.headers.project
+  if (!project) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({error: "Project name is required!"}),
+    };
+  }
+  const connect_sid = e.headers.connect_sid
+
+  const fetch = require("node-fetch").default;
+  const url = `https://scrapbox.io/api/pages/${project}`;
+  const ops = {
+    headers: {
+      cookie: connect_sid
+    }
+  }
+  const res = await fetch(url, ops);
   const data = await res.json();
+  if (res.status !== 200) {
+    return {
+      statusCode: res.status,
+      body: JSON.stringify(data),
+    }
+  }
   console.log(data.projectName + " : " + data.count);
   return {
     statusCode: 200,
