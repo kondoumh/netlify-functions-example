fd5b314c95def0427d2749fa53c7abece3a66396
kondoumh <kondoumh@gmail.com>
2020-11-28 09:37:19 +0900

    add header

--- resources/api/hello.js
+++ resources/api/hello.js
@@ -3,6 +3,9 @@ exports.handler = async (e, c) => {
   console.log(c);
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "text/plain",
+    },
     body: 'Hello World\n',
   };
 };
