78c1e59eedf7ec5c2b74284f84c39a010a13ad8d
kondoumh <kondoumh@gmail.com>
2022-07-23 16:34:47 +0900

    add context

---
+++ netlify/edge-functions/use-context.ts
@@ -0,0 +1,12 @@
+import { Context } from "netlify:edge";
+
+export default async (req: Request, { next, geo }: Context) => {
+  const res = await next();
+  let text = await res.text();
+
+  if (geo.country.code === "JP") {
+    text = text.replaceAll("Hello", "こんにちは");
+  }
+
+  return new Response(text, res);
+};
